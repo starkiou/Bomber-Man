7e453dd5cf0cb864d4d27639fcec88e92683a7f7
milo <milosorelcpt@gmail.com>
2026-03-30 15:22:35 +0200

    feat(layout): implement SceneManager for view switching

--- ClientSide/src/main/java/clientside/Bomberman.java
+++ ClientSide/src/main/java/clientside/Bomberman.java
@@ -1,19 +1,18 @@
 package clientside;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class Bomberman extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Bomberman.class.getResource("login-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        SceneManager.getInstance().setStage(stage);
         stage.setTitle("Bomberman \uD83D\uDCA3");
-        stage.setScene(scene);
+
+        SceneManager.getInstance().loadScene("login-view.fxml");
+
         stage.show();
     }
 }
