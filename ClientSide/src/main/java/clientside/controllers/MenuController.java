0895d39dbb7af435570446885faa7e8dadda8a8d
milo <milosorelcpt@gmail.com>
2026-03-30 15:43:56 +0200

    feat(network): add connection try-catch with error alerts, need the server to work inorder to test it further

--- ClientSide/src/main/java/clientside/controllers/MenuController.java
+++ ClientSide/src/main/java/clientside/controllers/MenuController.java
@@ -1,7 +1,10 @@
 package clientside.controllers;
 
+import clientside.SceneManager;
+import clientside.network.NetworkManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
@@ -15,25 +18,37 @@ public class MenuController {
 
     @FXML
     public void onConnectButtonClick(ActionEvent actionEvent) {
-        debugText.setText("Connexion en cours...");
+        debugText.setText("Connexion in progress...");
         debugText.setTextFill(Color.RED);
 
         String username = usernameField.getText();
         if (username.isBlank() || username.equals("")) {
-            debugText.setText("Username can't be blank");
+            showError("Erreur", "Username can't be blank !");
             return;
         } else if (username.length() >= 30) {
-            debugText.setText("Username too long");
+            showError("Erreur", "Username too long");
             return;
         }
 
         try{
-            
+            NetworkManager.getInstance().connect("127.0.0.1", 12345, username);
+            SceneManager.getInstance().loadScene("lobby-list.fxml");
         }catch(Exception e){
-
+            showError("Erreur", e.getMessage());
+            e.printStackTrace();
         }
 
 
 
+
     }
+
+    private void showError(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
 }
