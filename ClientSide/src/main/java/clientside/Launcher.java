d515efb5b007f891650178232da015557d615488
milo <milosorelcpt@gmail.com>
2026-03-30 15:34:49 +0200

    feat(username filter): added filter, you cant have a username too long (>30 char), or blank

--- ClientSide/src/main/java/clientside/Launcher.java
+++ ClientSide/src/main/java/clientside/Launcher.java
@@ -7,4 +7,3 @@ public class Launcher {
         Application.launch(Bomberman.class, args);
     }
 }
-    
--- ClientSide/src/main/java/clientside/controllers/MenuController.java
+++ ClientSide/src/main/java/clientside/controllers/MenuController.java
@@ -17,5 +17,23 @@ public class MenuController {
     public void onConnectButtonClick(ActionEvent actionEvent) {
         debugText.setText("Connexion en cours...");
         debugText.setTextFill(Color.RED);
+
+        String username = usernameField.getText();
+        if (username.isBlank() || username.equals("")) {
+            debugText.setText("Username can't be blank");
+            return;
+        } else if (username.length() >= 30) {
+            debugText.setText("Username too long");
+            return;
+        }
+
+        try{
+            
+        }catch(Exception e){
+
+        }
+
+
+
     }
 }
