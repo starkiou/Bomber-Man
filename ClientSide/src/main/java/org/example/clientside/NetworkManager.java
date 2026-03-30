0a76d9a11b669d5a751625c9ebe8b41196a2bb8f
milo <milosorelcpt@gmail.com>
2026-03-30 14:02:48 +0200

    chore: updated comment

--- ClientSide/src/main/java/org/example/clientside/NetworkManager.java
+++ ClientSide/src/main/java/org/example/clientside/NetworkManager.java
@@ -1,5 +1,36 @@
 package org.example.clientside;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class NetworkManager {
-    
+    private static NetworkManager instance;
+    private Socket socket;
+    private ObjectOutputStream out;
+    private String nickname;
+
+    // Singleton : une seule instance pour toute l'appli
+    public static NetworkManager getInstance() {
+        if (instance == null) instance = new NetworkManager();
+        return instance;
+    }
+
+    public void connect(String host, int port, String user) throws IOException {
+        this.nickname = user;
+        this.socket = new Socket(host, port);
+        this.out = new ObjectOutputStream(socket.getOutputStream());
+
+        // Lancer le thread qui écoute le serveur ici
+        new Thread(new NetworkListener(socket)).start();
+    }
+
+    public void sendMessage(Object msg) {
+        try {
+            out.writeObject(msg);
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
