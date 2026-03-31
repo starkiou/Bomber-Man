cff4f214c88c840c893edc39acd7dda40b8a6fbe
HEYBERGER <nathanael.heyberger@etu.unicaen.fr>
2026-03-31 09:23:07 +0200

    Add(MessageSerializer): create MessageSerializer file

--- Common/src/main/java/network/message/Message.java
+++ Common/src/main/java/network/message/Message.java
@@ -1,9 +1,7 @@
 package network.message;
 
 public interface Message {
-	public byte[] serialize();
 	
-	public Message deserialize(byte[] bytedObject);
 	
 	public MessageType getMessageType();
 }
---
+++ Common/src/main/java/network/message/MessageSerializer.java
@@ -0,0 +1,18 @@
+package network.message;
+
+public class MessageSerializer {
+	public byte[] serialize() {
+		
+		
+		
+		return null;
+		
+	}
+	
+	public Message deserialize(byte[] bytedObject) {
+		
+		return null;
+		
+	};
+
+}
