7c7492db182759bbf9f4f81b7badfe14aac78ceb
HEYBERGER <nathanael.heyberger@etu.unicaen.fr>
2026-03-31 12:45:36 +0200

    Fix(RoomThread): correction of inexistant class

--- ServerSide/src/main/java/org/example/RoomThread.java
+++ ServerSide/src/main/java/org/example/RoomThread.java
@@ -41,7 +41,7 @@ public class RoomThread extends Thread {
 		}
 	}
 	
-	private void broadcast(Message message) {
+	private void broadcast() {
 		for(ClientHandler client : listClient) {
 //			client.send();
 		}
