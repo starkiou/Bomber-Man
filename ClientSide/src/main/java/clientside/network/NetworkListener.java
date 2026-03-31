8d7fd0844411dd3b0b0329dd3d5756bcb7d20a05
milo <milosorelcpt@gmail.com>
2026-03-31 10:43:14 +0200

    feat(NetworkListener): added the Networklistener

--- ClientSide/src/main/java/clientside/network/NetworkListener.java
+++ ClientSide/src/main/java/clientside/network/NetworkListener.java
@@ -1,4 +1,38 @@
 package clientside.network;
 
-public class NetworkListener {
+import java.io.ObjectInputStream;
+import java.net.Socket;
+
+public class NetworkListener implements Runnable {
+    private final Socket socket;
+    private boolean running = true;
+
+    public NetworkListener(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try (ObjectInputStream in = new ObjectInputStream(socket.getInputStream())) {
+            while (running) {
+                // On attend un message du serveur (bloquant)
+                Object message = in.readObject();
+
+                handleMessage(message); //On traite le mess dedans
+            }
+        } catch (Exception e) {
+            System.err.println("Connexion perdue avec le serveur.");
+            e.printStackTrace();
+        }
+    }
+
+    private void handleMessage(Object msg) {
+        //TODO logique de redirection vers les controleurs pour l'affichage des infos recues
+
+        System.out.println("Message reçu du serveur : " + msg);
+    }
+
+    public void stop() {
+        this.running = false;
+    }
 }
