1c8ec690dae68bde74313f1d48abebd100deb234
lylian <montals.lylian2@gmail.com>
2026-03-31 13:49:09 +0200

    feat(player): implement movement with collision detection

--- Common/src/main/java/model/entity/Bomb.java
+++ Common/src/main/java/model/entity/Bomb.java
@@ -5,7 +5,7 @@ public class Bomb extends Entity{
     private int radius;
     private int explosionDelay;
 
-    public Bomb(int ID, double x, double y, boolean isSolid, int ownerID, int radius, int explosionDelay) {
+    public Bomb(int ID, int x, int y, boolean isSolid, int ownerID, int radius, int explosionDelay) {
         super(ID, x, y, isSolid);
         this.ownerID = ownerID;
         this.radius = radius;
