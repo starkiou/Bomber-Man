8f64922f16700b87f44aafdff26d245fdfb44de4
lylian <montals.lylian2@gmail.com>
2026-04-01 11:51:56 +0200

    feat(AIPlayer): implement all shared methods used by AI

--- Common/src/main/java/model/aiPlayer/AIFactory.java
+++ Common/src/main/java/model/aiPlayer/AIFactory.java
@@ -3,9 +3,7 @@ package model.aiPlayer;
 
 public class AIFactory {
 
-    public static AIPlayer create(Strategy strategy,
-                                  int id, int x, int y,
-                                  int hp, double speed, int maxBombs) {
+    public static AIPlayer create(Strategy strategy, int id, int x, int y, int hp, double speed, int maxBombs) {
         return switch (strategy) {
             case AGGRESSIVE  -> new AggressiveAI(id, x, y, hp, speed, maxBombs);
             case SURVIVALIST -> new SurvivalistAI(id, x, y, hp, speed, maxBombs);
